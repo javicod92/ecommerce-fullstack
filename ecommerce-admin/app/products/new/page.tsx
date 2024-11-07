63c541230524eed0a4e353a6aa3bd3643fb37ee9
javicod92 <javi.javier.jc@gmail.com>
2024-11-06 21:37:39 -0300

    Adding toastify

--- ecommerce-admin/app/products/new/page.tsx
+++ ecommerce-admin/app/products/new/page.tsx
@@ -1,23 +1,57 @@
+"use client";
+import { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+
+  const notify = () =>
+    toast.success("Product added successfully", {
+      position: "top-right",
+      autoClose: 3000, // Tiempo de duración en ms
+    });
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    notify();
+  }
+
   return (
-    <div className="">
+    <form onSubmit={handleSubmit}>
       <h1>New Product</h1>
       <fieldset className="border">
         <legend>Product Name</legend>
-        <input type="text" placeholder="Product Name" />
+        <input
+          type="text"
+          placeholder="Product Name"
+          value={title}
+          onChange={(e) => setTitle(e.currentTarget.value)}
+        />
       </fieldset>
       <fieldset>
         <legend>Description</legend>
         <textarea
           placeholder="Description"
           className="h-[200px] resize-none"
+          value={description}
+          onChange={(e) => setDescription(e.currentTarget.value)}
         ></textarea>
       </fieldset>
       <fieldset>
         <legend>Price</legend>
-        <input type="number" min={0} placeholder="Price in $USD" />
+        <input
+          type="number"
+          min={0}
+          placeholder="Price in $USD"
+          value={price}
+          onChange={(e) => setPrice(e.currentTarget.value)}
+        />
       </fieldset>
       <button className="btn-primary">Save Changes</button>
-    </div>
+      <ToastContainer />
+    </form>
   );
 }
